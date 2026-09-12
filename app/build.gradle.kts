fd8b014ce768f107c29389735f3eae2e6f3c161a
Ergouf <fjl1107464507@gmail.com>
2026-09-12 22:52:58 +0800

    ci: find APK path and bump 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ergouf.gecis"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.2.2"
+        versionCode = 9
+        versionName = "0.2.3"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
